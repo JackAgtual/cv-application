700f64c3e05cbdb487ab98e44b526bd65d331604
Jack Agtual <jaagtual@gmail.com>
2023-04-30 10:54:30 -0700

    Adding delete button to work experience and education experience

--- src/components/EducationExperienceForm.js
+++ src/components/EducationExperienceForm.js
@@ -7,14 +7,17 @@ export default class EducationExperienceForm extends Component {
 
     render() {
         return (
-            <form>
-                <label htmlFor="schoolName">School:</label>
-                <input type="text" id="schoolName"></input>
-                <label htmlFor="degreeName">Degree:</label>
-                <input type="text" id="degreeName"></input>
-                <label htmlFor="completionDate">Completion Date:</label>
-                <input type="text" id="completionDate"></input>
-            </form>
+            <div>
+                <form>
+                    <label htmlFor="schoolName">School:</label>
+                    <input type="text" id="schoolName"></input>
+                    <label htmlFor="degreeName">Degree:</label>
+                    <input type="text" id="degreeName"></input>
+                    <label htmlFor="completionDate">Completion Date:</label>
+                    <input type="text" id="completionDate"></input>
+                </form>
+                <button>Delete</button>
+            </div>
         )
     }
 }
--- src/components/WorkExperienceForm.js
+++ src/components/WorkExperienceForm.js
@@ -7,18 +7,21 @@ export default class WorkExperienceForm extends Component {
 
     render() {
         return (
-            <form>
-                <label htmlFor="jobTitle">Title:</label>
-                <input type="text" id="jobTitle"></input>
-                <label htmlFor="companyName">Company:</label>
-                <input type="text" id="companyName"></input>
-                <label htmlFor="from">From:</label>
-                <input type="text" id="from"></input>
-                <label htmlFor="to">To:</label>
-                <input type="text" id="to"></input>
-                <label htmlFor="description">Description:</label>
-                <input type="text" id="description"></input>
-            </form>
+            <div>
+                <form>
+                    <label htmlFor="jobTitle">Title:</label>
+                    <input type="text" id="jobTitle"></input>
+                    <label htmlFor="companyName">Company:</label>
+                    <input type="text" id="companyName"></input>
+                    <label htmlFor="from">From:</label>
+                    <input type="text" id="from"></input>
+                    <label htmlFor="to">To:</label>
+                    <input type="text" id="to"></input>
+                    <label htmlFor="description">Description:</label>
+                    <input type="text" id="description"></input>
+                </form>
+                <button>Delete</button>
+            </div>
         )
     }
 }
