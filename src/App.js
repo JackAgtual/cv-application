6c6706eb963124c806679e6939592330a1a84d65
Jack Agtual <jaagtual@gmail.com>
2023-04-29 23:01:46 -0700

    Adding initial WorkExperienceForm

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import PersonalInfoForm from './components/PersonalInfoForm'
+import WorkExperienceForm from './components/WorkExperienceForm';
 
 function App() {
   return (
     <div className="App">
       <PersonalInfoForm />
+      <WorkExperienceForm />
     </div>
   );
 }
---
+++ src/components/WorkExperienceForm.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+export default class WorkExperienceForm extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="formHeader">Work Experience</div>
+                <form>
+                    <label htmlFor="jobTitle">Title:</label>
+                    <input type="text" id="jobTitle"></input>
+                    <label htmlFor="companyName">Company:</label>
+                    <input type="text" id="companyName"></input>
+                    <label htmlFor="from">From:</label>
+                    <input type="text" id="from"></input>
+                    <label htmlFor="to">To:</label>
+                    <input type="text" id="to"></input>
+                    <label htmlFor="description">Description:</label>
+                    <input type="text" id="description"></input>
+                </form>
+            </div >
+        )
+    }
+}
