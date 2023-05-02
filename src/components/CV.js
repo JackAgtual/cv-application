677a394c87400ea59aded346b863df5c3fc93eb0
Jack Agtual <jaagtual@gmail.com>
2023-05-01 18:06:51 -0700

    Styling CV

--- src/components/CV.js
+++ src/components/CV.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react"
+import CVCSS from '../styles/CV.module.css'
+import uniqid from 'uniqid'
 
 export default class CV extends Component {
     constructor(props) {
@@ -8,30 +10,32 @@ export default class CV extends Component {
     render() {
         const { personalInfo, workExperience, educationExperience } = this.props
         return (
-            <div>
-                <div className="personalInfo">
-                    <div className="name">{`${personalInfo.firstName} ${personalInfo.lastName}`}</div>
-                    <div className="contact">{`${personalInfo.email} | ${personalInfo.phoneNumber}`}</div>
+            <div className={CVCSS.cv}>
+                <div className={CVCSS.personalInfo}>
+                    <div className={CVCSS.name}>{`${personalInfo.firstName} ${personalInfo.lastName}`}</div>
+                    <div>{`${personalInfo.email} | ${personalInfo.phoneNumber}`}</div>
                 </div>
-                <div className="workExperience">
+                <div className={CVCSS.workExperience}>
+                    <div className={CVCSS.sectionHeader}>Work Experience</div>
                     {workExperience.map(job => {
                         return (
-                            <div className="job">
-                                <div className="jobTitle">{job.title}</div>
-                                <div className="company">{job.company}</div>
-                                <div className="jobDates">{`${job.from} - ${job.to}`}</div>
-                                <div className="jobDescription">{job.description}</div>
+                            <div key={uniqid()} className={CVCSS.experience}>
+                                <div className={CVCSS.experienceTitle}>{job.title}</div>
+                                <div>{`${job.from} - ${job.to}`}</div>
+                                <div className={CVCSS.companyName}>{job.company}</div>
+                                <div className={CVCSS.jobDescription}>{job.description}</div>
                             </div>
                         )
                     })}
                 </div>
-                <div className="educationExperience">
+                <div>
+                    <div className={CVCSS.sectionHeader}>Education</div>
                     {educationExperience.map(school => {
                         return (
-                            <div className="education">
-                                <div className="schoolName">{school.school}</div>
-                                <div className="degree">{school.degree}</div>
-                                <div className="completionDate">{school.completionDate}</div>
+                            <div key={uniqid()} className={CVCSS.experience}>
+                                <div className={CVCSS.experienceTitle}>{school.school}</div>
+                                <div>{school.completionDate}</div>
+                                <div>{school.degree}</div>
                             </div>
                         )
                     })}
